3217c7c36715811ea16f8de3cb5b0dec14bd9d27
deadem <deadem@gmail.com>
2022-12-17 12:48:57 +0300

    фикс ошибки сборки

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,6 +1,5 @@
 export { ChatPage } from './chat-page';
 export { ErrorPage } from './error-page';
 export { LoginPage } from './login-page';
-export { ProfileEditPage } from './profile-edit-page';
 export { ProfilePage } from './profile-page';
 export { RegistrationPage } from './registration-page';
