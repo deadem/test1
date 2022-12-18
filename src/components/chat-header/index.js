2f5f520427327167ee6abb020cab1394aae39b9f
deadem <deadem@gmail.com>
2022-12-18 17:25:30 +0300

    добавлена иконка удаления

--- src/components/chat-header/index.js
+++ src/components/chat-header/index.js
@@ -1,11 +1,12 @@
 import Handlebars from 'handlebars/runtime';
 import './chat-header.scss';
 import plus from '../../assets/plus_icon.svg';
+import cross from '../../assets/cross_icon.svg';
 export { default as ChatHeader } from './chat-header.hbs';
 
 Handlebars.registerHelper('chat-header-menu', () => {
   return [
     { icon: plus, text: 'Добавить пользователя', page: 'chat-user-add' },
-    { icon: plus, text: 'Удалить пользователя', page: 'chat-user-add' },
+    { icon: cross, text: 'Удалить пользователя', page: 'chat-user-add' },
   ];
 });
