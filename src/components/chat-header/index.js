6c8b2fbae65a347b03204fc6fec2abaac9f1069b
deadem <deadem@gmail.com>
2022-12-18 17:02:06 +0300

    добавлена иконка плюса

--- src/components/chat-header/index.js
+++ src/components/chat-header/index.js
@@ -1,10 +1,11 @@
 import Handlebars from 'handlebars/runtime';
 import './chat-header.scss';
+import plus from '../../assets/plus_icon.svg';
 export { default as ChatHeader } from './chat-header.hbs';
 
 Handlebars.registerHelper('chat-header-menu', () => {
   return [
-    { text: 'Добавить пользователя', page: 'chat-user-add' },
-    { text: 'Удалить пользователя', page: 'chat-user-add' },
+    { icon: plus, text: 'Добавить пользователя', page: 'chat-user-add' },
+    { icon: plus, text: 'Удалить пользователя', page: 'chat-user-add' },
   ];
 });
