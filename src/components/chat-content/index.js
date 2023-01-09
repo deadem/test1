a829f74e64e68c6686d917cae9735015195eacfd
deadem <deadem@gmail.com>
2023-01-09 19:12:12 +0300

    Четыре хелпера заменены на один с параметром, текст вынесен в отдельный js-файл (чисто json не подошёл из-за хитрого импорта картинки)

--- src/components/chat-content/index.js
+++ src/components/chat-content/index.js
@@ -1,29 +1,6 @@
 import Handlebars from 'handlebars/runtime';
 import './chat-content.scss';
+import { message } from './chat-content';
 export { default as ChatContent } from './chat-content.hbs';
 
-Handlebars.registerHelper('chat-content-text', () => {
-  return [
-    "Привет! Смотри, тут всплыл интересный кусок лунной космической истории — НАСА в какой-то момент попросила Хассельблад адаптировать модель SWC для полетов на Луну. Сейчас мы все знаем что астронавты летали с моделью 500 EL — и к слову говоря, все тушки этих камер все еще находятся на поверхности Луны, так как астронавты с собой забрали только кассеты с пленкой.",
-    "Хассельблад в итоге адаптировал SWC для космоса, но что-то пошло не так и на ракету они так никогда и не попали. Всего их было произведено 25 штук, одну из них недавно продали на аукционе за 45000 евро.",
-    ];
-});
-
-Handlebars.registerHelper('chat-content-text-error', () => {
-  return [
-    "<a href page='error'>Ссылка на страницу с ошибкой</a>"
-  ];
-});
-
-Handlebars.registerHelper('chat-content-image', () => {
-  return new URL(
-    '../../assets/chat-message-image.png',
-    import.meta.url
-  );
-});
-
-Handlebars.registerHelper('chat-content-text-my', () => {
-    return [
-      "Круто!",
-      ];
-  });
+Handlebars.registerHelper('chat-content', (section) => message[section]);
