ccd0d584f747162867c12f76cc229889c08da16c
deadem <deadem@gmail.com>
2023-01-15 14:11:45 +0300

    подключена выдача index.html на запрос GET /

--- server.js
+++ server.js
@@ -1,9 +1,14 @@
 const express = require('express');
+const path = require("path");
 
 const app = express();
 const PORT = process.env.SERVER_PORT || 3000;
 
-app.use(express.static('./dist'));
+app.use(express.static(path.resolve(__dirname, "dist")));
+
+app.get("/", (_, res) => {
+  res.sendFile(path.resolve(__dirname, "dist", "index.html"));
+});
 
 app.listen(PORT, function () {
   console.log(`Example app listening on port ${PORT}!`);
