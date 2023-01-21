0be8215860aeea4a55496eb42a2ebe27b89c608a
deadem <deadem@gmail.com>
2023-01-22 01:02:23 +0300

    Block разгружен от сборки шаблона, убраны ненужные знания

--- src/handlebars.ts
+++ src/handlebars.ts
@@ -1,17 +1,39 @@
 import Handlebars, { HelperOptions } from 'handlebars';
+import { Block } from './utils/Block';
 
-interface BlockComponent {
-  id: number;
-  render(): void;
+export interface BlockComponent {
+  content(): Element;
 }
 
+export function compile(template: string, context: Record<string, unknown>) {
+  const data = { ...context, __children: [] as Array<{ component: Block, embed(node: DocumentFragment): void }> };
+  const html = Handlebars.compile(template)(data);
+  return { html, children: data.__children };
+}
+
+let uniqueId = 0; // Идентификатор текущего компонента. Используется во время рендеринга для подстановки дочерних компонентов.
+
+// NB: Не поддерживается перерендер компонентов с детьми, вида {{#A ...}}...{{/A}}
 export function registerComponent<T extends BlockComponent>(Component: { new (...args: Record<string, unknown>[]): T }) {
   Handlebars.registerHelper(Component.name,
     function (this: unknown, { hash, data, fn }: HelperOptions) {
       const component = new Component(hash);
-      (data.root.children = data.root.children || {})[component.id] = component;
+      const dataAttribute = `data-component-hbs-id="${++uniqueId}"`;
+
+      (data.root.__children = data.root.__children || []).push({ component, embed(node: DocumentFragment) {
+        // Ищем среди детей плейсхолдеры для размещения компонентов и заменяем заглушки на реальные компоненты
+        const placeholder = node.querySelector(`[${dataAttribute}]`);
+        if (!placeholder) {
+          throw new Error(`Can't find data-id for component ${component.constructor.name}`);
+        }
+
+        const element = component.content();
+        element.append(...Array.from(placeholder.childNodes));
+        placeholder.replaceWith(element);
+      }});
+
       const contents = fn ? fn(this) : '';
-      return `<div data-id="${component.id}">${contents}</div>`;
+      return `<div ${dataAttribute}>${contents}</div>`;
     },
   );
 }
--- src/utils/Block.ts
+++ src/utils/Block.ts
@@ -1,13 +1,10 @@
-import Handlebars from 'handlebars';
+import { compile } from '../handlebars';
 
 export abstract class Block {
-  public id = ++Block.currentIdCounter; // Идентификатор текущего компонента. Используется во время рендеринга для подстановки дочерних компонентов.
-
   protected abstract template: string; // Handlebars-шаблон текущего компонента.
   protected props: Record<string, unknown> = {}; // Свойства компонента. Будут переданы в шаблон во время рендеринга
 
-  private static currentIdCounter = 0; // Счётчик. Нужен для уникального идентификатора при создании дочерних компонентов
-  private children: Record<string, Block> = {}; // как таковой не нужен, храним только для того, чтобы было у кого вызывать unmount
+  private children: Block[] = []; // как таковой не нужен, храним только для того, чтобы было у кого вызывать unmount
   private element: Element | null = null; // Элемент в DOM, в который отрендерен этот компонент
 
   constructor(props: Record<string, unknown>) {
@@ -19,14 +16,17 @@ export abstract class Block {
     this.render();
   }
 
-  public render() {
+  public test() {
+    return this.children;
+  }
+
+  private render() {
     const fragment = this.compile();
 
     if (this.element) {
       this.element.replaceWith(fragment);
-    } else {
-      this.element = fragment;
     }
+    this.element = fragment;
   }
 
   public content(): HTMLElement {
@@ -37,25 +37,13 @@ export abstract class Block {
   }
 
   protected compile() {
-    const context = { ...this.props };
-    const html = Handlebars.compile(this.template)(context);
-    this.children = context.children as Record<string, Block> || {};
+    const { html, children } = compile(this.template, this.props);
+    this.children = children.map(child => child.component);
 
     const templateElement = document.createElement('template');
     templateElement.innerHTML = html;
     const fragment = templateElement.content;
-
-    // Ищем среди детей плейсхолдеры для размещения компонентов и заменяем заглушки на реальные компоненты
-    Object.values(this.children).forEach((component) => {
-      const placeholder = fragment.querySelector(`[data-id="${component.id}"]`);
-      if (!placeholder) {
-        throw new Error(`Can't find data-id for component ${component}`);
-      }
-
-      const element = component.content();
-      element.append(...Array.from(placeholder.childNodes));
-      placeholder.replaceWith(element);
-    });
+    children.forEach(child => child.embed(fragment));
 
     if (!fragment.firstElementChild || fragment.firstElementChild?.nextElementSibling) {
       console.error(fragment);
