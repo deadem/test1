5d4ba520e91a8430c570a45f565553d4e9fccde1
deadem <deadem@gmail.com>
2023-01-21 11:07:07 +0300

    подключён валидатор stylelint

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,18 @@
-import { resolve } from 'path';
+import { resolve, dirname } from 'path';
+import { fileURLToPath } from 'url';
 import { defineConfig } from 'vite';
 import eslint from 'vite-plugin-eslint';
+import stylelint from 'vite-plugin-stylelint';
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
 
 export default defineConfig({
   root: resolve(__dirname, 'src'),
   build: {
     outDir: resolve(__dirname, 'dist'),
   },
-  plugins: [eslint()],
+  plugins: [eslint(), stylelint({
+    cacheLocation: resolve(__dirname, '.cache', 'stylelint-cache')
+  })],
 });
