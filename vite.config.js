eb135f0d04b5d4624279ab9766eea6c833e3369b
deadem <deadem@gmail.com>
2023-01-21 12:28:48 +0300

    выключено предупреждение линтера о неизвестной встроенной node-переменной

--- vite.config.js
+++ vite.config.js
@@ -1,15 +1,18 @@
 import { defineConfig } from 'vite';
 import checker from 'vite-plugin-checker';
 
+// eslint-disable-next-line no-undef
+const dirname = __dirname;
+
 export default defineConfig({
   plugins: [
     checker({
       typescript: true,
       eslint: {
-        lintCommand: `eslint --cache --cache-location=${__dirname}/.cache/ "src/**/*.{js,ts}"`,
+        lintCommand: `eslint --cache --cache-location=${dirname}/.cache/ "src/**/*.{js,ts}"`,
       },
       stylelint: {
-        lintCommand: `stylelint --cache --cache-location=${__dirname}/.cache/stylelint-cache src/**/*.{css,scss}`,
+        lintCommand: `stylelint --cache --cache-location=${dirname}/.cache/stylelint-cache src/**/*.{css,scss}`,
       },
     })
   ],
