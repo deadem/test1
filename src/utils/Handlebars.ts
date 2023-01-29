8a2b4f53f0b77e1c2932fa91dfe801c8907fdeef
deadem <deadem@gmail.com>
2023-01-29 17:14:11 +0300

    скорректированы отступы в исходнике Handlebars

--- src/utils/Handlebars.ts
+++ src/utils/Handlebars.ts
@@ -29,31 +29,30 @@ type ComponentType<T extends BlockComponentClass<T>> = { new (props: Constructor
 
 export function registerComponent<T extends BlockComponentClass<T>>(Component: ComponentType<T>) {
   Handlebars.registerHelper(Component.componentName, function (this: unknown, { hash, data, fn }: HelperOptions) {
-      const component = new Component(hash);
-      const dataAttribute = `data-component-hbs-id="${++uniqueId}"`;
+    const component = new Component(hash);
+    const dataAttribute = `data-component-hbs-id="${++uniqueId}"`;
 
-      if ('ref' in hash) {
-        // если в свойствах компонента кто-то хочет получить на него ссылку - сохраним
-        (data.root__refs = data.root.__refs || {})[hash.ref] = component;
+    if ('ref' in hash) {
+      // если в свойствах компонента кто-то хочет получить на него ссылку - сохраним
+      (data.root__refs = data.root.__refs || {})[hash.ref] = component;
+    }
+
+    (data.root.__children = data.root.__children || []).push({ component, embed(node: DocumentFragment) {
+      // Ищем среди детей плейсхолдеры для размещения компонентов и заменяем заглушки на реальные компоненты
+      const placeholder = node.querySelector(`[${dataAttribute}]`);
+      if (!placeholder) {
+        throw new Error(`Can't find data-id for component ${Component.componentName}`);
       }
 
-      (data.root.__children = data.root.__children || []).push({ component, embed(node: DocumentFragment) {
-        // Ищем среди детей плейсхолдеры для размещения компонентов и заменяем заглушки на реальные компоненты
-        const placeholder = node.querySelector(`[${dataAttribute}]`);
-        if (!placeholder) {
-          throw new Error(`Can't find data-id for component ${Component.componentName}`);
-        }
+      const element = component.element();
+      element.append(...Array.from(placeholder.childNodes));
+      placeholder.replaceWith(element);
+    }});
 
-        const element = component.element();
-        element.append(...Array.from(placeholder.childNodes));
-        placeholder.replaceWith(element);
-      }});
-
-      // NB: Не поддерживается перерендер компонентов с детьми, вида {{#A ...}} ... {{/A}}
-      const contents = fn ? fn(this) : '';
-      return `<div ${dataAttribute}>${contents}</div>`;
-    },
-  );
+    // NB: Не поддерживается перерендер компонентов с детьми, вида {{#A ...}} ... {{/A}}
+    const contents = fn ? fn(this) : '';
+    return `<div ${dataAttribute}>${contents}</div>`;
+  });
 }
 
 export function registerHelpers() {
