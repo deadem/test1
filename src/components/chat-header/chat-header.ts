d1142551b1a8b7f18d797177aa980a77acf1a7fa
deadem <deadem@gmail.com>
2023-01-31 21:26:47 +0300

    добавлено включение меню по клику на многоточии в заголовке чата

--- src/components/chat-header/chat-header.ts
+++ src/components/chat-header/chat-header.ts
@@ -1,20 +1,45 @@
 import './chat-header.scss';
-// import plus from '../../assets/plus_icon.svg';
-// import cross from '../../assets/cross_icon.svg';
+import plus from '../../assets/plus_icon.svg';
+import cross from '../../assets/cross_icon.svg';
 import template from './chat-header.hbs?raw';
 import { Block } from '../../utils/Block';
 
 interface Props {
+  menu?: { icon: string; text: string; }[] | undefined;
 }
 
-export class ChatHeader extends Block<Props> {
+type Refs = {
+  button: HTMLElement;
+}
+
+export class ChatHeader extends Block<Props, Refs> {
   static componentName = 'ChatHeader';
   protected template = template;
+  protected override events = {
+    button: {
+      click: (e: Event) => this.showMenu(e)
+    }
+  };
+
+  private showMenu(e: Event) {
+    e.stopPropagation();
+    this.setProps({
+      menu: [
+        { icon: plus, text: 'Добавить пользователя' },
+        { icon: cross, text: 'Удалить пользователя' },
+      ]
+    });
+  }
+
+  protected override componentDidMount(): void {
+    // Пока не реализован функционал меню, тут сами управляем его показом-скрытием, исключительно для демонстрации динамики.
+    const handler = () => {
+      if (this.props.menu) {
+        document.removeEventListener('click', handler);
+        this.setProps({ menu: undefined });
+      }
+    };
+    document.addEventListener('click', handler);
+  }
 }
 
-// Handlebars.registerHelper('chat-header-menu', () => {
-//   return [
-//     { icon: plus, text: 'Добавить пользователя', page: 'chat-user-add' },
-//     { icon: cross, text: 'Удалить пользователя', page: 'chat-user-add' },
-//   ];
-// });
